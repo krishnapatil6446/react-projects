d280d98979911dcf3ff03e10fd1f2d8e20ca2cbb
Krishna Patil <krishnapatil6446@gmail.com>
2025-01-24 20:56:32 +0530

    saved the router progress

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-green-100'>React-Router</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import React from 'react'
+import {BrowserRouter, ReactProvider, createBrowserRouter} from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Layout from './layout.jsx'
+
+const router = createBrowserRouter([
+  { path :'/', 
+    element:<Layout/>,
+    children:[{
+      path:'',
+      element:<Home/>
+    },{}]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ReactProvider/>
+  </StrictMode>,
+)
